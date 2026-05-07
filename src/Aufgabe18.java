833f0e69cfbda1b4034721ba6c50afa9d0ea10c9
Danish <binkamar@th-brandenburg.de>
2026-05-07 14:57:54 +0200

    Version 2: Aufgabe 18 old switch statement with constants

--- src/Aufgabe18.java
+++ src/Aufgabe18.java
@@ -9,26 +9,48 @@ public class Aufgabe18 {
         final String summer = "Summer";
         final String autumn = "Autumn";
         final String winter = "Winter";
+
+        final int JANUARY = 1;
+        final int FEBRUARY = 2;
+        final int MARCH = 3;
+        final int APRIL = 4;
+        final int MAY = 5;
+        final int JUNE = 6;
+        final int JULY = 7;
+        final int AUGUST = 8;
+        final int SEPTEMBER = 9;
+        final int OCTOBER = 10;
+        final int NOVEMBER = 11;
+        final int DECEMBER = 12;
+
         int option;
 
         System.out.print("Enter number of a month: \n");
         option = scanner.nextInt();
 
         switch (option) {
-            case 3,4,5:
-                System.out.println("Month " + option + " is" + spring + ".");
+            case MARCH:
+            case APRIL:
+            case MAY:
+                System.out.println("Month " + option + " is " + spring + ".");
                 break;
-            case 6,7,8:
-                System.out.println("Month " + option + " is" + summer + ".");
+            case JUNE:
+            case JULY:
+            case AUGUST:
+                System.out.println("Month " + option + " is " + summer + ".");
                 break;
-            case 9,10,11:
-                 System.out.println("Month " + option + " is" + autumn + ".");
-                 break;
-            case 12,1,2:
-                 System.out.println("Month " + option + " is" + winter + ".");
-                 break;
+            case SEPTEMBER:
+            case OCTOBER:
+            case NOVEMBER:
+                System.out.println("Month " + option + " is " + autumn + ".");
+                break;
+            case DECEMBER:
+            case JANUARY:
+            case FEBRUARY:
+                System.out.println("Month " + option + " is " + winter + ".");
+                break;
             default:
-                 System.out.println("No Season!");
+                System.out.println("No Season!");
         }
 
         scanner.close();
